024b850b811018271c51c609f9130b545497b50b
Ezequiel Rangel <chekelinho@gmail.com>
2025-04-28 19:05:54 -0700

    Adds metadata and OG image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from "next";
 import SiteBody from "~/components/site-body";
 import { SiteHeader } from "~/components/site-header";
+
+export const metadata: Metadata = {
+  title: "Job Snap | A CV/Job Compatibility Analysis Tool",
+  description:
+    "Our CV-Job Compatibility Analyzer uses advanced AI technology to provide you with precise, data-driven insights into how well your qualifications align with job requirements—all from simple text input.",
+};
 
 export default function Home() {
   return (
