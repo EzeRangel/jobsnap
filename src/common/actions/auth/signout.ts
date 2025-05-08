e3a7ab64ecb546ac5a93485e697f278b6ff7d504
Ezequiel Rangel <chekelinho@gmail.com>
2025-05-08 11:44:40 -0700

    Implement supabase auth functions

---
+++ src/common/actions/auth/signout.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import { createClient } from "~/lib/supabase/server";
+
+export async function signOut() {
+  const supabase = await createClient();
+  const { error } = await supabase.auth.signOut();
+}
---
+++ src/common/actions/auth/signup.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { zfd } from "zod-form-data";
+import { actionClient } from "~/lib/safe-action/create-action-client";
+import { createClient } from "~/lib/supabase/server";
+
+const schema = zfd.formData({
+  email: zfd.text(
+    z
+      .string({ required_error: "Please type your email" })
+      .email({ message: "Type a correct email" })
+  ),
+  password: zfd.text(z.string({ required_error: "Please type a password" })),
+});
+
+export const signup = actionClient.schema(schema).action(
+  async ({ parsedInput }) => {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.signUp(parsedInput);
+
+    /** Fix this */
+    if (error) {
+      throw error;
+    }
+  },
+  {
+    onSuccess() {
+      redirect("/");
+    },
+  }
+);
