b2065b12860bc9fc1d26c60539f6e3294a1c48b9
Ezequiel Rangel <chekelinho@gmail.com>
2025-05-13 07:03:33 -0700

    Refactor SignupForm component and enhance SignupPage layout with additional features

--- src/app/signup/_components/signup-form.tsx
+++ src/app/signup/_components/signup-form.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { cn } from "~/lib/utils";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
+import { Card, CardContent } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import useMutation from "~/hooks/useMutation";
@@ -23,17 +17,20 @@ export function SignupForm({
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
-        <CardHeader className="text-center">
-          <CardTitle className="text-xl">Create an account</CardTitle>
-          <CardDescription>
-            Let&apos;s get started. Fill in the details below to create your
-            account.
-          </CardDescription>
-        </CardHeader>
         <CardContent>
           <form action={execute}>
             <div className="grid gap-6">
               <div className="grid gap-6">
+                <div className="grid grid-cols-2 gap-3">
+                  <div className="grid gap-3">
+                    <Label htmlFor="name">Name</Label>
+                    <Input id="name" name="name" required />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="lastname">Last Name</Label>
+                    <Input id="lastname" name="lastname" required />
+                  </div>
+                </div>
                 <div className="grid gap-3">
                   <Label htmlFor="email">Email</Label>
                   <Input
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,17 +1,61 @@
-import { FileSearchIcon } from "lucide-react";
+import {
+  FileSearchIcon,
+  FoldersIcon,
+  LockKeyholeOpenIcon,
+  TrendingUpIcon,
+} from "lucide-react";
 import { SignupForm } from "./_components/signup-form";
+import Link from "next/link";
 
 export default function SignupPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <FileSearchIcon className="size-4" />
-          </div>
-          Job Snap
-        </a>
-        <SignupForm />
+      <div className="flex w-full max-w-4xl flex-row gap-6">
+        <div className="flex flex-col gap-6">
+          <Link href="/">
+            <div className="bg-primary text-primary-foreground flex size-8 items-center justify-center rounded-md">
+              <FileSearchIcon className="size-6" />
+            </div>
+          </Link>
+          <h1 className="font-bold text-3xl">Create a free account</h1>
+          <p className="text-muted-foreground">
+            Level Up Your Job Hunt with Pro Tools and Insights
+          </p>
+          <ul className="space-y-6">
+            <li className="flex flex-row gap-2">
+              <LockKeyholeOpenIcon className="shrink-0" />
+              <p className="text-sm text-muted-foreground">
+                <strong className="block text-base text-foreground mb-3">
+                  Unlock Full AI Insights
+                </strong>
+                Get deeper, more personalized analysis to boost your job match
+                potential.
+              </p>
+            </li>
+            <li className="flex flex-row gap-2">
+              <FoldersIcon className="shrink-0" />
+              <p className="text-sm text-muted-foreground">
+                <strong className="block text-base text-foreground mb-3">
+                  Save & Manage Multiple Resumes
+                </strong>
+                Keep all your CV versions in one place for easy access and
+                comparison.
+              </p>
+            </li>
+            <li className="flex flex-row gap-2">
+              <TrendingUpIcon className="shrink-0" />
+              <p className="text-sm text-muted-foreground">
+                <strong className="block text-base text-foreground mb-3">
+                  Track Your Progress Over Time
+                </strong>
+                See how your resume improves with every tweak and suggestion.
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <SignupForm />
+        </div>
       </div>
     </div>
   );
