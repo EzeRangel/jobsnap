441ea68ec660292b23d1ada5354e1f77b9e5bce5
Ezequiel Rangel <chekelinho@gmail.com>
2025-05-19 20:31:16 -0700

    Refactor UserDTO to make profile optional and update supabase types for better null handling

--- src/types/UserProfile.ts
+++ src/types/UserProfile.ts
@@ -2,7 +2,7 @@ import { User } from "@supabase/supabase-js";
 import { Database } from "./supabase";
 
 export interface UserDTO extends Pick<User, "id" | "email"> {
-  profile: UserProfile;
+  profile?: UserProfile;
 }
 
 export type UserProfile = Database["public"]["Tables"]["UsersProfile"]["Row"];
